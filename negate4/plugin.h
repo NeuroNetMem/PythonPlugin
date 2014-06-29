b739fe325282ba801a373d72367ec25b5a2dc504
Francesco Battaglia <fpbattaglia@gmail.com>
2014-06-29 22:01:56 +0200

    Adding plugin files

---
+++ negate4/plugin.h
@@ -0,0 +1,26 @@
+#ifndef __PYX_HAVE__negate4__plugin
+#define __PYX_HAVE__negate4__plugin
+
+
+#ifndef __PYX_HAVE_API__negate4__plugin
+
+#ifndef __PYX_EXTERN_C
+  #ifdef __cplusplus
+    #define __PYX_EXTERN_C extern "C"
+  #else
+    #define __PYX_EXTERN_C extern
+  #endif
+#endif
+
+__PYX_EXTERN_C DL_IMPORT(void) pluginFunction(float *, int, int);
+__PYX_EXTERN_C DL_IMPORT(int) pluginisready(void);
+
+#endif /* !__PYX_HAVE_API__negate4__plugin */
+
+#if PY_MAJOR_VERSION < 3
+PyMODINIT_FUNC initplugin(void);
+#else
+PyMODINIT_FUNC PyInit_plugin(void);
+#endif
+
+#endif /* !__PYX_HAVE__negate4__plugin */
