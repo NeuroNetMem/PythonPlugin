713058de6917b449d04c9e3f000841da7bd3ff80
Francesco Battaglia <fpbattaglia@gmail.com>
2014-07-23 16:30:28 +0200

    added mechanism for parameter value callback into python. Refactored python side so that editable code is now in a class

--- negate4/plugin.h
+++ negate4/plugin.h
@@ -14,9 +14,11 @@
 
 __PYX_EXTERN_C DL_IMPORT(void) pluginStartup(void);
 __PYX_EXTERN_C DL_IMPORT(int) getParamNum(void);
+__PYX_EXTERN_C DL_IMPORT(void) getParamConfig(struct ParamConfig *);
 __PYX_EXTERN_C DL_IMPORT(void) pluginFunction(float *, int, int);
 __PYX_EXTERN_C DL_IMPORT(int) pluginisready(void);
-__PYX_EXTERN_C DL_IMPORT(void) getParamConfig(struct ParamConfig *);
+__PYX_EXTERN_C DL_IMPORT(void) setIntParam(char *, int);
+__PYX_EXTERN_C DL_IMPORT(void) setFloatParam(char *, int);
 
 #endif /* !__PYX_HAVE_API__negate4__plugin */
 
--- negate4/plugin.pyx
+++ negate4/plugin.pyx
@@ -2,6 +2,34 @@ cimport numpy as np
 import numpy as np
 from cython cimport view
 
+
+class Negate(object):
+    def __init__(self):
+        self.enabled = 1
+
+    def startup(self):
+        self.enabled = 1
+
+    def plugin_name(self):
+        return "Negate"
+
+    def isReady(self):
+        return 1
+
+    def paramConfig(self):
+        return (("toggle", "enabled" , True),)
+
+    def bufferfunction(self, n_arr):
+        #print "plugin start"
+        cdef float mult
+        mult = 1. - 2. * self.enabled
+        n_arr[4,:] = mult * n_arr[4,:]
+        #print "plugin end"
+
+
+pluginOp = Negate()
+
+
 cdef extern from "../../Source/Processors/PythonParamConfig.h":
     enum paramType:
         TOGGLE, INT_SET, FLOAT_RANGE
@@ -19,31 +47,35 @@ cdef extern from "../../Source/Processors/PythonParamConfig.h":
         char *name
         int isEnabled
 
+
+
 cdef public void pluginStartup():
-    pass
+    pluginOp.startup()
 
 cdef public int getParamNum():
-    return 3
-
-
-
-cdef public void pluginFunction(float *buffer, int nChans, int nSamples):
-    print "plugin start"
-
-    #cdef view.array c_arr = <float[:nChans, :nSamples]> buffer
-    n_arr = np.asarray(<np.float32_t[:nChans, :nSamples]> buffer)
-    #c_arr[4,:] = c_arr[4,:] * 2
-    n_arr[4,:] = - n_arr[4,:]
-    print "plugin end"
-
-
-cdef public int pluginisready():
-    return 1
+    return len(pluginOp.paramConfig())
 
 cdef public void getParamConfig(ParamConfig *params):
-    params[0].type = TOGGLE
-    params[0].name = "Apple"
-    params[1].type = TOGGLE
-    params[1].name = "Banana"
-    params[2].type = TOGGLE
-    params[2].name = "Cherry"
+    ppc = pluginOp.paramConfig()
+    for i in range(len(ppc)):
+        par = ppc[i]
+        if par[0] == "toggle":
+            params[0].type = TOGGLE
+            params[0].name = par[1]
+            params[0].isEnabled = par[2]
+        #TODO other types of commands
+
+cdef public void pluginFunction(float *buffer, int nChans, int nSamples):
+    n_arr = np.asarray(<np.float32_t[:nChans, :nSamples]> buffer)
+    pluginOp.bufferfunction(n_arr)
+
+cdef public int pluginisready():
+    return pluginOp.isReady()
+
+cdef public void setIntParam(char *name, int value):
+    print "In Python: ", name, ": ", value
+    setattr(pluginOp, name, value)
+
+cdef public void setFloatParam(char *name, int value):
+    print "In Python: ", name, ": ", value
+    setattr(pluginOp, name, value)
