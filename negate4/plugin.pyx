dc2d55c055193ad1a1a70477ea1b8a11fc13bfae
Francesco Battaglia <fpbattaglia@gmail.com>
2014-07-28 17:28:12 +0200

    Started the event communication machine

--- negate4/plugin.pyx
+++ negate4/plugin.pyx
@@ -6,41 +6,6 @@ from cython cimport view
 
 
 class Negate(object):
-    def __init__(self):
-        self.enabled = 1
-        self.channel = 1
-        self.mult = 1.
-        self.range_min = 0.2
-        self.range_max = 5
-        self.start_value = 1
-
-    def startup(self):
-        self.enabled = 1
-
-    def plugin_name(self):
-        return "Negate"
-
-    def is_ready(self):
-        return 1
-
-    def param_config(self):
-        chan_labels = range(16);
-        return (("toggle", "enabled" , True),
-                ("int_set", "channel", chan_labels),
-                ("float_range", "mult", self.range_min, self.range_max, self.start_value))
-
-    def bufferfunction(self, n_arr):
-        #print "plugin start"
-        cdef float mult
-        cdef int chan
-        chan = self.channel
-        mult = self.mult
-
-        n_arr[chan,:] = mult * n_arr[chan,:]
-        #print "plugin end"
-
-
-pluginOp = Negate()
 
 
 cdef extern from "../../Source/Processors/PythonParamConfig.h":
@@ -66,15 +31,81 @@ cdef extern from "../../Source/Processors/PythonParamConfig.h":
         float startValue
 
 
+# struct PythonEvent {
+#     unsigned char type;
+#     int sampleNum;
+#     unsigned char eventId;
+#     unsigned char eventChannel;
+#     unsigned char numBytes;
+#     unsigned char *eventData;
+#     struct PythonEvent *nextEvent
+# };
+
+
+cdef extern from "../../Source/Processors/PythonEvent.h":
+    struct PythonEvent:
+        unsigned char type
+        int sampleNum
+        unsigned char eventId
+        unsigned char eventChannel
+        unsigned char numBytes
+        unsigned char *eventData
+        PythonEvent *nextEvent
+
+    def __init__(self):
+        self.enabled = 1
+        self.channel = 1
+        self.mult = 1.
+        self.range_min = 0.2
+        self.range_max = 5
+        self.start_value = 1
+        self.idx = 1
+
+    def startup(self):
+        self.enabled = 1
+
+    def plugin_name(self):
+        return "Negate"
+
+    def is_ready(self):
+        return 1
+
+    def param_config(self):
+        chan_labels = range(16)
+        return (("toggle", "enabled" , True),
+                ("int_set", "channel", chan_labels),
+                ("float_range", "mult", self.range_min, self.range_max, self.start_value))
+
+    def bufferfunction(self, n_arr):
+        #print "plugin start"
+        cdef float mult
+        cdef int chan
+        chan = self.channel
+        mult = self.mult
+
+        n_arr[chan,:] = mult * n_arr[chan,:]
+        print self.idx
+        self.idx += 1
+        #print "plugin end"
+
+
+cdef Negate pluginOp = Negate()
+
+############## here starts the C++ interface
+
+
 cdef public void pluginStartup():
-    pluginOp.startup()
+    cdef Negate po = pluginOp
+    po.startup()
 
 cdef public int getParamNum():
-    return len(pluginOp.param_config())
+    cdef Negate po = pluginOp
+    return len(po.param_config())
 
 cdef public void getParamConfig(ParamConfig *params):
     cdef int *ent
-    ppc = pluginOp.param_config()
+    cdef Negate po = pluginOp
+    ppc = po.param_config()
     for i in range(len(ppc)):
         par = ppc[i]
         if par[0] == "toggle":
@@ -98,17 +129,24 @@ cdef public void getParamConfig(ParamConfig *params):
 
 
 
-cdef public void pluginFunction(float *buffer, int nChans, int nSamples):
+cdef public void pluginFunction(float *buffer, int nChans, int nSamples, PythonEvent *events):
     n_arr = np.asarray(<np.float32_t[:nChans, :nSamples]> buffer)
-    pluginOp.bufferfunction(n_arr)
+    #pluginOp.set_events(events)
+    #pm2 = PluginModule(pm)
+    cdef Negate po = pluginOp
+    po.set_events(events)
+    po.bufferfunction(n_arr)
 
 cdef public int pluginisready():
-    return pluginOp.is_ready()
+    cdef Negate po = pluginOp
+    return po.is_ready()
 
 cdef public void setIntParam(char *name, int value):
+    cdef Negate po = pluginOp
     print "In Python: ", name, ": ", value
-    setattr(pluginOp, name, value)
+    setattr(po, name, value)
 
 cdef public void setFloatParam(char *name, float value):
     # print "In Python: ", name, ": ", value
-    setattr(pluginOp, name, value)
+    cdef Negate po = pluginOp
+    setattr(po, name, value)
