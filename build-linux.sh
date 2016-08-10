76d499233e4d1cde4fe242791d929f6f1b2f894c
Francesco Battaglia <fpbattaglia@gmail.com>
2016-08-10 16:39:25 +0200

    fixed build-linux.sh symlink

--- build-linux.sh
+++ build-linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ln -s PythonPlugin/Source/ ../plugin-GUI/Source/Plugins/PythonPlugin
+ln -s ../../../PythonPlugin/PythonPlugin/Source/ ../plugin-GUI/Source/Plugins/PythonPlugin
 cd ../plugin-GUI/Builds/Linux/
 make -f Makefile.plugins
 
