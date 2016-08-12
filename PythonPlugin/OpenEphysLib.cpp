e9346f55aa06240fda664ee3d0658e2c229f8de9
Francesco Battaglia <fpbattaglia@gmail.com>
2016-08-12 15:56:39 +0200

    included sources and sinks. Removed ZMQPlugins which do not belong here.

--- PythonPlugin/OpenEphysLib.cpp
+++ PythonPlugin/OpenEphysLib.cpp
@@ -35,9 +35,12 @@
 #endif
 
 #include "PythonFilter.h"
+#include "PythonSource.h"
+#include "PythonSink.h"
+
 using namespace Plugin;
 //Number of plugins defined on the library. Can be of different types (Processors, RecordEngines, etc...)
-#define NUM_PLUGINS 1
+#define NUM_PLUGINS 3
 
 extern "C" EXPORT void getLibInfo(Plugin::LibraryInfo* info)
 {
@@ -60,29 +63,22 @@ extern "C" EXPORT int getPluginInfo(int index, Plugin::PluginInfo* info)
             info->processor.type = Plugin::FilterProcessor; //Type of processor. Can be FilterProcessor, SourceProcessor, SinkProcessor or UtilityProcessor. Specifies where on the processor list will appear
             info->processor.creator = &(Plugin::createProcessor<PythonFilter>); //Class factory pointer. Replace "ExampleProcessor" with the name of your class.
             break;
-            /**
-             Examples for other plugin types
-             
-             For a RecordEngine, which provides formats for recording data
-             case x:
-             info->type = Plugin::RecordEnginePlugin;
-             info->recordEngine.name = "Record Engine Name";
-             info->recordEngine.creator = &(Plugin::createRecordEngine<RecordEngineClassName>);
-             break;
-             
-             For a DataThread, which allows to use the existing SourceNode to connect to an asynchronous data source, such as acquisition hardware
-             case x:
-             info->type = Plugin::DatathreadPlugin;
-             info->dataThread.name = "Source name"; //Name that will appear on the processor list
-             info->dataThread.creator = &createDataThread<DataThreadClassName>;
-             
-             For a FileSource, which allows importing data formats into the FileReader
-             case x:
-             info->type = Plugin::FileSourcePlugin;
-             info->fileSource.name = "File Source Name";
-             info->fileSource.extensions = "xxx;xxx;xxx"; //Semicolon separated list of supported extensions. Eg: "txt;dat;info;kwd"
-             info->fileSource.creator = &(Plugin::createFileSource<FileSourceClassName>);
-             **/
+        case 1:
+            info->type = Plugin::ProcessorPlugin; //Type of plugin. See "Source/Processors/PluginManager/OpenEphysPlugin.h" for complete info about the different type structures
+            //For processor
+            info->processor.name = "Python Source"; //Processor name shown in the GUI
+            info->processor.type = Plugin::SourceProcessor; //Type of processor. Can be FilterProcessor, SourceProcessor, SinkProcessor or UtilityProcessor. Specifies where on the processor list will appear
+            info->processor.creator = &(Plugin::createProcessor<PythonSource>); //Class factory pointer. Replace "ExampleProcessor" with the name of your class.
+
+            break;
+        case 2:
+            info->type = Plugin::ProcessorPlugin; //Type of plugin. See "Source/Processors/PluginManager/OpenEphysPlugin.h" for complete info about the different type structures
+            //For processor
+            info->processor.name = "Python Sink"; //Processor name shown in the GUI
+            info->processor.type = Plugin::SinkProcessor; //Type of processor. Can be FilterProcessor, SourceProcessor, SinkProcessor or UtilityProcessor. Specifies where on the processor list will appear
+            info->processor.creator = &(Plugin::createProcessor<PythonSink>); //Class factory pointer. Replace "ExampleProcessor" with the name of your class.
+            
+            break;
         default:
             return -1;
             break;
