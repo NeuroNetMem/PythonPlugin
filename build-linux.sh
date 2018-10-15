78d129b9653ca08936a98a77930fc95326d1cee2
Francesco Battaglia <fpbattaglia@gmail.com>
2018-10-15 12:28:54 +0200

    automatic detection of python configuration in linux

--- build-linux.sh
+++ build-linux.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-export CONDA_HOME=/usr/local/anaconda
-export CONFIG=Release
+export CONDA_HOME=$(python -c "from distutils import sysconfig; \
+import re; \
+z = sysconfig.get_config_var('prefix'); \
+print(z)")
+
+export PYTHON_VERSION=$(python -c "from distutils import sysconfig; \
+import re; \
+z = sysconfig.get_config_var('BLDLIBRARY'); \
+m = re.search(r'-l(.*)', z); \
+print(m.group(1))")
+
+export CONFIG=Debug
 ln -s ../../../PythonPlugin/PythonPlugin/ ../plugin-GUI/Source/Plugins/PythonPlugin
 cd ../plugin-GUI/Builds/Linux/
 make -f Makefile.plugins
