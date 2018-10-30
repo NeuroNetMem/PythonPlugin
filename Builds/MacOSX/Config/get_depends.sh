313af54f9fb713d7318846483e3445fbba6bd883
Francesco Battaglia <fpbattaglia@gmail.com>
2018-10-30 09:18:58 +0100

    Fixed Mac Config script/template

--- Builds/MacOSX/Config/get_depends.sh
+++ Builds/MacOSX/Config/get_depends.sh
@@ -16,7 +16,7 @@ z = sysconfig.get_config_var('BLDLIBRARY'); \
 m = re.search(r'-l(.*)', z); \
 print(m.group(1))")
 
-#echo $PYTHON_DIR
-#echo $PYTHON_VERSION
+echo $PYTHON_DIR
+echo $PYTHON_VERSION
 
-sed -e 's/#PYTHON_DIR#/${PYTHON_DIR}/' -e 's/#PYTHON_VERSION#/${PYTHON_VERSION}/' <${CONFIG_DIR}/Plugin.xcconfig.tmpl >${CONFIG_DIR}/Plugin.xcconfig
+sed -e "s%#PYTHON_DIR#%${PYTHON_DIR}%" -e "s%#PYTHON_VERSION#%${PYTHON_VERSION}%" <${CONFIG_DIR}/Plugin.xcconfig.tmpl >${CONFIG_DIR}/Plugin.xcconfig
