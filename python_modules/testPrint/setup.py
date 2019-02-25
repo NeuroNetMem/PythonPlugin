fec20ccef2b643cd90de715afdded8a62de60156
Mark Schatza <markschatza@gmail.com>
2019-02-25 12:11:30 -0600

    Fixed Setup.py

--- python_modules/testPrint/setup.py
+++ python_modules/testPrint/setup.py
@@ -3,11 +3,8 @@ from Cython.Build import cythonize
 from Cython.Compiler import Options
 import numpy
 
-Options.embed = "main"
-
-
 setup(
         name= "testPrint",
-        ext_modules = cythonize(Extension('testPrint',sources=["testPrint.pyx"],export_symbols=['pluginStartup','pluginisready','getParamNum','getParamConfig','pluginFunction','eventFunction','spikeFunction','setIntParam','setFloatParam','getIntParam','getFloatParam',])),
+        ext_modules = cythonize(Extension('testPrint',sources=["testPrint.pyx"],export_symbols=['pluginStartup','pluginisready','getParamNum','getParamConfig','pluginFunction','eventFunction','spikeFunction','setIntParam','setFloatParam','getIntParam','getFloatParam'])),
         include_dirs = [numpy.get_include()]
         )
