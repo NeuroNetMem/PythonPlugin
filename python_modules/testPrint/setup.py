0edeed605bf31df1647e8bc9032d43156a30a074
Mark Schatza <markschatza@gmail.com>
2019-02-25 11:23:36 -0600

    Export pyd functions for windows.

--- python_modules/testPrint/setup.py
+++ python_modules/testPrint/setup.py
@@ -8,6 +8,6 @@ Options.embed = "main"
 
 setup(
         name= "testPrint",
-        ext_modules = cythonize("testPrint.pyx"),
+        ext_modules = cythonize(Extension('testPrint',sources=["testPrint.pyx"],export_symbols=['pluginStartup','pluginisready','getParamNum','getParamConfig','pluginFunction','eventFunction','spikeFunction','setIntParam','setFloatParam','getIntParam','getFloatParam',])),
         include_dirs = [numpy.get_include()]
         )
--- python_modules/testPrint/testPrint.pyx
+++ python_modules/testPrint/testPrint.pyx
@@ -1,4 +1,3 @@
-# distutils: language=c++
 import sys
 import numpy as np
 cimport numpy as np
@@ -9,26 +8,32 @@ isDebug = False
 class testPrint(object):
         def __init__(self):
                 """initialize object data"""
-                print('hello world\n\n')
+                print('hello world_init\n\n')
                 self.Enabled = 1
         def startup(self, sr):
                 """to be run upon startup"""
+                print('hello world_startup\n\n')
         def plugin_name(self):
                 """tells OE the name of the program"""
+                print('hello world_name\n\n')
                 return "testPrint"
         def is_ready(self):
                 """tells OE everything ran smoothly"""
+                print('hello world_is_ready\n\n')
                 return self.Enabled
         def param_config(self):
                 """return button, sliders, etc to be present in the editor OE side"""
                 return []
         def bufferfunction(self, n_arr):
                 """Access to voltage data buffer. Returns events"""
+                print('hello world_buffer\n\n')
                 events = []
                 return events
         def handleEvents(eventType,sourceID,subProcessorIdx,timestamp,sourceIndex):
+          print('hello world_handle_events\n\n')
                 """handle events passed from OE"""
         def handleSpike(self, electrode, sortedID, n_arr):
+          print('hello world_handle_spike\n\n')
                 """handle spikes passed from OE"""
 
 
