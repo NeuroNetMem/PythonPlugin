2c2e7863085c374f977c73c245f8d75f0e0b9bcf
Ethan Blackwood <ethanbblackwood@gmail.com>
2019-05-28 13:58:10 -0500

    Fix setup.py of new new-style plugins

--- python_modules/spwdouble/setup.py
+++ python_modules/spwdouble/setup.py
@@ -1,9 +1,24 @@
 from distutils.core import setup, Extension
 from Cython.Build import cythonize
 import numpy
+import runpy
+
+cfg = runpy.run_path('../.config.py')
 
 setup(
-        name= "spwdouble",
-        ext_modules = cythonize(Extension('spwdouble',sources=["spwdouble.pyx"],export_symbols=['pluginStartup','pluginisready','getParamNum','getParamConfig','pluginFunction','eventFunction','spikeFunction','setIntParam','setFloatParam','getIntParam','getFloatParam'])),
-        include_dirs = [numpy.get_include()]
-        )
+        name="spwdouble",
+        include_dirs=[numpy.get_include(), cfg['PYTHON_PLUGIN_SRC_DIR']],
+        ext_modules=cythonize(Extension('spwdouble', sources=["spwdouble.pyx"], export_symbols=[
+                'pluginStartup',
+                'pluginisready',
+                'getParamNum',
+                'getParamConfig',
+                'pluginFunction',
+                'eventFunction',
+                'spikeFunction',
+                'setIntParam',
+                'setFloatParam',
+                'getIntParam',
+                'getFloatParam'
+        ]))
+)
--- python_modules/spwrandom/setup.py
+++ python_modules/spwrandom/setup.py
@@ -1,9 +1,24 @@
 from distutils.core import setup, Extension
 from Cython.Build import cythonize
 import numpy
+import runpy
+
+cfg = runpy.run_path('../.config.py')
 
 setup(
-        name= "spwrandom",
-        ext_modules = cythonize(Extension('spwrandom',sources=["spwrandom.pyx"],export_symbols=['pluginStartup','pluginisready','getParamNum','getParamConfig','pluginFunction','eventFunction','spikeFunction','setIntParam','setFloatParam','getIntParam','getFloatParam'])),
-        include_dirs = [numpy.get_include()]
-        )
+        name="spwrandom",
+        include_dirs=[numpy.get_include(), cfg['PYTHON_PLUGIN_SRC_DIR']],
+        ext_modules=cythonize(Extension('spwrandom', sources=["spwrandom.pyx"], export_symbols=[
+                'pluginStartup',
+                'pluginisready',
+                'getParamNum',
+                'getParamConfig',
+                'pluginFunction',
+                'eventFunction',
+                'spikeFunction',
+                'setIntParam',
+                'setFloatParam',
+                'getIntParam',
+                'getFloatParam'
+        ]))
+)
