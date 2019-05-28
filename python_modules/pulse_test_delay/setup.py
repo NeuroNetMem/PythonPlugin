e4bc66db05b92d333154b5e7e1aa81e0c2c4dcf2
Francesco Battaglia <fpbattaglia@gmail.com>
2019-05-28 14:01:54 +0200

    updates all setup.py's form "new" plugins

--- python_modules/pulse_test_delay/setup.py
+++ python_modules/pulse_test_delay/setup.py
@@ -1,6 +1,10 @@
 from distutils.core import setup, Extension
 from Cython.Build import cythonize
 import numpy
+import runpy
+
+cfg = runpy.run_path('../.config.py')
+
 
 setup(
     name="pulse_test_delay",
@@ -8,5 +12,5 @@ setup(
                                     export_symbols=['pluginStartup', 'pluginisready', 'getParamNum', 'getParamConfig',
                                                     'pluginFunction', 'eventFunction', 'spikeFunction', 'setIntParam',
                                                     'setFloatParam', 'getIntParam', 'getFloatParam'])),
-    include_dirs=[numpy.get_include()]
+    include_dirs=[numpy.get_include(), cfg['PYTHON_PLUGIN_SRC_DIR']]
 )
--- python_modules/spwdouble/setup.py
+++ python_modules/spwdouble/setup.py
@@ -1,9 +1,13 @@
 from distutils.core import setup, Extension
 from Cython.Build import cythonize
 import numpy
+import runpy
+
+cfg = runpy.run_path('../.config.py')
+
 
 setup(
         name= "spwdouble",
         ext_modules = cythonize(Extension('spwdouble',sources=["spwdouble.pyx"],export_symbols=['pluginStartup','pluginisready','getParamNum','getParamConfig','pluginFunction','eventFunction','spikeFunction','setIntParam','setFloatParam','getIntParam','getFloatParam'])),
-        include_dirs = [numpy.get_include()]
+        include_dirs = [numpy.get_include(), cfg['PYTHON_PLUGIN_SRC_DIR']]
         )
